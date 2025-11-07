02620bc7f4700183faa4f5f7399089239ee689e0
Adriano Oliviero <adrianoliviero23@gmail.com>
2025-11-07 12:44:08 +0100

    Initial Commit

---
+++ PicSolve/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PicSolve
+//
+//  Created by Adriano Oliviero on 07/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PicSolve/PicSolveApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicSolveApp.swift
+//  PicSolve
+//
+//  Created by Adriano Oliviero on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PicSolveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
